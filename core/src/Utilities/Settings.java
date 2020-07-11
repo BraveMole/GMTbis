f725a07d0628eabcad73c802ecab62bd42bd0a69
Bastien <bsouchu@gmail.com>
2020-07-12 00:19:07 +0200

    Obstacle rectanle renderer

--- core/src/Utilities/Settings.java
+++ core/src/Utilities/Settings.java
@@ -5,4 +5,5 @@ public class Settings {
     public static float metersToPixelRatio=0.1f;
     public static float metersOnScreen =5;
     public static float playerSpeed =10;
+    public static String layerName = "Ground";
 }
